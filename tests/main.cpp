19bae606d67e24cd0d02eaaeaafac6468ffe06a9
Stan S <jarlofearls@gmail.com>
2016-04-13 20:42:42 -0400

    Initial Commit

---
+++ tests/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tests
+//
+//  Created by Stan Slupecki on 4/13/16.
+//  Copyright © 2016 Stan Slupecki. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
